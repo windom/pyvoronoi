13d66fcd456f8e22bfd5d0d02da2cdcdc7592951
Miklos Gabor <wndmearle@yahoo.com>
2014-08-11 20:03:06 +0300

    Rajzolasi lehetosegek kiemelese egy helyre

--- main.py
+++ main.py
@@ -1,14 +1,65 @@
 import random
 import tkinter as tk
+import math
 
 import utils
 import graphics as gr
 import voronoi as vr
 
-MAX_WIDTH = 700
-MAX_HEIGHT = 600
-PADDING = 20
+###########################################################################
 
+def setup_points():
+    relaxation = 0
+
+    #random.seed(100)
+    #points = gr.generate_points(50, XRANGE, YRANGE)
+
+    #points = spiral_points(radius_i=0.8, radius_i2=0.02, turns=170)
+    #points = spiral_points(radius_i=2, radius_i2=0, turns=170)
+
+    #points = circle_points(radius_i=20, radius_i2=-1, angle_fractions=9, turns=30, antisymm=0)
+    points = circle_points(radius_i=20, radius_i2=0, angle_fractions=9, turns=15, antisymm=0)
+    #points = circle_points(radius_i=20, radius_i2=-1, angle_fractions=18, turns=15, antisymm=3)
+
+    #points = grid_points(size=50)
+
+    return (relaxation, points)
+
+def setup_drawing():
+    draw_mode = 'voronoi';
+    #draw_mode = 'tri-center';
+    #draw_mode = 'tri-delaunay';
+
+    #draw_points = True
+    draw_points = False
+
+    #area_bounds = None
+    area_bounds = (100,1700)
+
+    outline_color = None
+
+    #outline_color = utils.rgb_to_hex(0,0,0)
+    #get_color = lambda _: (255,255,255)
+
+    get_color = gr.weighted_color((20,20,138), (140,140,198))
+    outline_color = utils.rgb_to_hex(20,20,138)
+
+    #get_color = gr.weighted_color((236,57,50), (148,18,18))
+    #outline_color = utils.rgb_to_hex(148,18,18)
+
+    #get_color = gr.weighted_color((38,63,93), (184,210,221))
+    #outline_color = utils.rgb_to_hex(38,63,93)
+
+    return (draw_mode, draw_points, area_bounds, outline_color, get_color)
+
+###########################################################################
+
+MAX_WIDTH = 670
+MAX_HEIGHT = 670
+PADDING = 10
+
+XRANGE = (PADDING, MAX_WIDTH - PADDING)
+YRANGE = (PADDING, MAX_HEIGHT - PADDING)
 
 class DrawingApp(tk.Frame):
 
@@ -20,9 +71,6 @@ class DrawingApp(tk.Frame):
         canvas = tk.Canvas(self)
         canvas.pack(fill=tk.BOTH, expand=1)
         self.canvas = gr.MyCanvas(canvas)
-
-        # w = tk.Text(self, height=5)
-        # w.pack(fill=tk.X)
 
 def createRoot(width, height, title='Drawing'):
     root = tk.Tk()
@@ -40,55 +88,96 @@ def createRoot(width, height, title='Drawing'):
 
     return root
 
+def spiral_points(radius_i, radius_i2, turns):
+    points = []
+    angle = 0
+    radius = 0
+    for _ in range(turns):
+        pt = gr.Point(MAX_WIDTH/2 + math.cos(angle) * radius,
+                      MAX_HEIGHT/2 + math.sin(angle) * radius)
+        angle += math.pi / 9
+        radius += radius_i
+        radius_i += radius_i2
+        points.append(pt)
+    return points
+
+def circle_points(radius_i, radius_i2, angle_fractions, turns, antisymm):
+    points = []
+    iangle = 0
+    iradius = radius_i
+    for _ in range(turns):
+        angle = iangle
+        radius = iradius
+        for _ in range(angle_fractions*2-antisymm):
+            pt = gr.Point(MAX_WIDTH/2 + math.cos(angle) * radius,
+                          MAX_HEIGHT/2 + math.sin(angle) * radius)
+            angle += math.pi / angle_fractions
+            radius += 0.001
+            points.append(pt)
+        iradius = radius + radius_i
+        radius_i += radius_i2
+        iangle += math.pi / (angle_fractions*2)
+    return points
+
+def grid_points(size):
+    points = []
+    dx = 0
+    for y in range(YRANGE[0], YRANGE[1], size):
+        dx = size/2-dx
+        for x in range(XRANGE[0], XRANGE[1], size):
+            points.append(gr.Point(x + dx, y))
+    return points
 
 def calculate():
-    #random.seed(233)
+    (relaxation, points) = setup_points()
 
-    points = 100
-    relaxation = 0
-    xrange = (PADDING, MAX_WIDTH - PADDING)
-    yrange = (PADDING, MAX_HEIGHT - PADDING)
-
-    vor = vr.Voronoi(gr.generate_points(points, xrange, yrange))
-    vor.compact_polygons(xrange, yrange)
+    vor = vr.Voronoi(points)
+    vor.compact_polygons(XRANGE, YRANGE)
 
     for _ in range(relaxation):
         vor = vor.floyd_relaxation()
-        vor.compact_polygons(xrange, yrange)
+        vor.compact_polygons(XRANGE, YRANGE)
 
     return vor
-
 
 def draw(canvas, data):
     vor = data
 
-    #polys = vor.polygons.values()
+    (draw_mode, draw_points, area_bounds, outline_color, get_color) = setup_drawing()
 
-    xrange = (PADDING, MAX_WIDTH - PADDING)
-    yrange = (PADDING, MAX_HEIGHT - PADDING)
-    polys = [gr.Polygon(tri.a, tri.b, tri.c) for tri in vor.triangles \
-                if all(map(lambda p: xrange[0] <= p.x <= xrange[1] and yrange[0] <= p.y <= yrange[1], [tri.a, tri.b, tri.c]))]
+    if draw_mode == 'voronoi':
+        polys = vor.polygons.values()
+    elif draw_mode == 'tri-center':
+        polys = vor.polygons.values()
+        newpolys = []
+        for poly in polys:
+            newpolys.extend([gr.Polygon(p1,p2,poly.centroid) for (p1,p2) in zip(poly.points, poly.points[1:] + poly.points[0:1])])
+        polys = [poly for poly in newpolys if all(map(lambda p: XRANGE[0] <= p.x <= XRANGE[1] and YRANGE[0] <= p.y <= YRANGE[1], poly.points))]
+    else:
+        polys = [gr.Polygon(tri.a, tri.b, tri.c) for tri in vor.triangles \
+                    if all(map(lambda p: XRANGE[0] <= p.x <= XRANGE[1] and YRANGE[0] <= p.y <= YRANGE[1], [tri.a, tri.b, tri.c]))]
 
-    maxarea = max(map(lambda poly: abs(poly.area), polys))
-    minarea = min(map(lambda poly: abs(poly.area), polys))
+    areas = list(map(lambda poly: abs(poly.area), polys))
+    if area_bounds:
+        areas = [area for area in areas if area_bounds[0] <= area <= area_bounds[1]]
 
-    outline_color = None
+    maxarea = max(areas)
+    minarea = min(areas)
+    avgarea = sum(areas) / len(polys)
 
-    get_color = gr.weighted_color((20,20,138), (140,140,198))
-    outline_color = utils.rgb_to_hex(20,20,138)
-
-    # get_color = gr.weighted_color((236,57,50), (148,18,18))
-    # outline_color = utils.rgb_to_hex(148,18,18)
-
-    #get_color = gr.weighted_color((38,63,93), (184,210,221))
-    #outline_color = utils.rgb_to_hex(38,63,93)
+    print("Areas: min={}, max={}, avg={}".format(minarea, maxarea, avgarea))
 
     for poly in polys:
-        weight = (abs(poly.area) - minarea) / (maxarea - minarea)
-        canvas.draw_polygon(poly, fill=utils.rgb_to_hex(*get_color(weight)), outline=outline_color)
+        area = abs(poly.area)
+        if (not area_bounds) or (area_bounds[0] <= area <= area_bounds[1]):
+            weight = (area - minarea) / (maxarea - minarea)
+            canvas.draw_polygon(poly, fill=utils.rgb_to_hex(*get_color(weight)), outline=outline_color)
+        else:
+            canvas.draw_polygon(poly, fill="#ffffff", outline=outline_color)
 
-    # for point in vor.points:
-    #     canvas.draw_point(point)
+    if draw_points:
+        for point in vor.points:
+            canvas.draw_point(point)
 
 def main():
     data = calculate()
